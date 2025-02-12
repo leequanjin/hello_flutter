de58421c8328c3be7bfc57b839186238da030327
Lee Quan Jin <130299519+leequanjin@users.noreply.github.com>
2025-02-12 14:44:29 +0800

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Hello Flutter'),
+          backgroundColor: Colors.blue,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            spacing: 8.0,
+            children: [
+              Text('Login'),
+              TextField(
+                keyboardType: TextInputType.name,
+                decoration: const InputDecoration(
+                  labelText: 'Username',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              TextField(
+                keyboardType: TextInputType.text,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              ElevatedButton(
+                  onPressed: (){},
+                  child: Text('Login')
+              ),
+              ElevatedButton(
+                  onPressed: (){},
+                  child: Text('Register')
+              ),
+            ],
+
+          ),
+        ),
+      ),
+    );
+  }
+}
